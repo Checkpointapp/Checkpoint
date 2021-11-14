01d6e1831f4c62f1b0f818e6f0c760343c3794e6
rav4s <ravidhruvishshah@gmail.com>
2021-11-13 21:01:09 -0600

    Don't show survey notif on survey page

--- checkpoint/src/components/Router.jsx
+++ checkpoint/src/components/Router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import CreateTask from "./CreateTask";
 import Tasks from './Tasks';
+import Survey from "./Survey";
 
 export default function Router() {
 
@@ -12,6 +13,7 @@ export default function Router() {
                {/* <Route exact path="/" component={MainPage}/>*/}
                 <Route exact path="/tasks" component={Tasks}/>
                 <Route exact path="/create-task" component={CreateTask}/>
+                <Route exact path="/registration-questions" component={Survey}/>
             </Switch>
         </BrowserRouter>
     </>
--- checkpoint/src/components/SurveyNotif.js
+++ checkpoint/src/components/SurveyNotif.js
@@ -2,12 +2,17 @@ import '../styles/main.css';
 import '../styles/survey.css';
 import firebase from "firebase/app";
 import { Button } from 'react-bootstrap';
+import { useHistory } from "react-router";
 import "firebase/database";
 import React, { useState, useEffect } from 'react';
 
 function SurveyNotif(props) {
-
+    let history = useHistory();
     const [text, setText] = useState(null);
+
+    async function onClick() {
+        history.push('/registration-questions');
+    }
 
     useEffect(() => {
         var userId = firebase.auth().currentUser.uid;
@@ -15,26 +20,30 @@ function SurveyNotif(props) {
         ref.once("value")
             .then(function(snapshot) {
                 var exists = snapshot.exists();
-                console.log(exists);
                 setText(exists);
             });
     }, []);
 
-    return (
-        <>
-        {text === false ?
+    if (history.location.pathname !== '/registration-questions') {
+        return (
             <>
-            <div className="survey-notification-container">
-                <div className="survey-notification">
-                    <p>Welcome! Please answer a few questions to customize your Checkpoint experience</p>
+            {text === false ?
+                <>
+                <div className="survey-notification-container">
+                    <div className="survey-notification">
+                        <p>Welcome! Please answer a few questions to customize your Checkpoint experience</p>
+                        <Button onClick={onClick}></Button>
+                    </div>
                 </div>
-            </div>
+                </>
+                :
+                null
+            }
             </>
-            :
-            null
-        }
-        </>
-    )
+        )
+    } else {
+        return null;
+    }
 
 }
 
