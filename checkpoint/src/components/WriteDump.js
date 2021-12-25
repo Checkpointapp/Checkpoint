0e533a51280bb0e0e1263607fcdbb84f98f11170
rav4s <ravidhruvishshah@gmail.com>
2021-12-25 12:50:32 +0530

    Start adding writing dump page

---
+++ checkpoint/src/components/WriteDump.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react"
+import firebase from "firebase/app";
+import { InputGroup, FormControl, Button, Form } from "react-bootstrap";
+import '../../src/styles/writedump.css'
+import "firebase/database";
+
+function WriteDump() {
+
+    return (
+      <>
+        <h1>Hello</h1>
+      </>
+  )
+}
+
+export default WriteDump;
