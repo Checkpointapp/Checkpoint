27584ca5aa1ebe8574434b8b282b5e28aeb05e4b
rav4s <ravidhruvishshah@gmail.com>
2021-12-29 16:25:15 +0530

    Added textbox for writedump (logic not impl)

--- checkpoint/src/components/WriteDump.js
+++ checkpoint/src/components/WriteDump.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react"
 import firebase from "firebase/app";
 import { InputGroup, FormControl, Button, Form } from "react-bootstrap";
-import '../../src/styles/writedump.css'
+import '../../src/styles/writedump.css';
 import "firebase/database";
 
 function WriteDump() {
 
     return (
       <>
-        <h1>Hello</h1>
+        <h1>Writing Dump 📝</h1>
+        <div className="textbox">
+            <textarea></textarea>
+        </div>
       </>
   )
 }
